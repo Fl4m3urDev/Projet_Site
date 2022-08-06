e311c45473568a9462de37b7d08aa0f6c86b4480
Fl4m3urDev <VoxFlameur22@gmail.com>
2022-08-06 13:55:41 +0200

    version retravaillé

--- script.js
+++ script.js
@@ -1,92 +1,144 @@
-window.addEventListener('load', function (){
+window.addEventListener('load', function () {
     console.log('Cette fonction est éxécutée une fois quand la page est chargée.');
 });
 
 const max = 3;
-const click = 32;
 
 var time = new Date().getHours();
 if (time < 7) {
-window.alert("Bonne nuit et Bienvenue Test !");
+    window.alert("Bonne nuit et Bienvenue Test !");
 } else if (time < 18) {
-window.alert("Bonjour et Bienvenue Test !");
+    window.alert("Bonjour et Bienvenue Test !");
 } else {
-window.alert("Bonsoir et Bienvenue Test !");
+    window.alert("Bonsoir et Bienvenue Test !");
 }
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-function getRandomInt(click) {
-    return Math.floor(Math.random() * click);
-}
-
-function ChangeImgClick(){
-    var tabImg = new Array ();
-    tabImg[0] = "images/anime_girls_sexy.png";
-    tabImg[1] = "images/ffn_girl.png";
-    tabImg[2] = "images/Sailor_Moon.png";
-    tabImg[3] = "images/TMM_Zoey.png";
-    tabImg[4] = "images/MD_Doremi.png";
-    tabImg[5] = "images/SC_Amu.png";
-    tabImg[6] = "images/Kilari.png";
-    tabImg[7] = "images/Lucie_PPP.png";
-    tabImg[8] = "images/FT_Lucy.png";
-    tabImg[9] = "images/Ayu_Tsukimiya.png";
-    tabImg[10] = "images/mkdm_tohru.png";
-    tabImg[11] = "images/The_Quintessential_Quintuplets.png";
-    tabImg[12] = "images/Nagatoro.gif";
-    tabImg[13] = "images/Marin_Kitagawa_MDUD.png";
-    tabImg[14] = "images/Marin_Kitagawa_2_MDUD.png";
-    tabImg[15] = "images/Komi_San.png";
-    tabImg[16] = "images/Kizuna_AI.png";
-    tabImg[17] = "images/Hatsune_Miku.png";
-    tabImg[18] = "images/Haruhi_Suzumiya.png";
-    tabImg[19] = "images/Konata_Izumi.png";
-    tabImg[20] = "images/Yazawa_Nico.png";
-    tabImg[21] = "images/Kirino_Kousaka.png";
-    tabImg[22] = "images/Doki_Doki_Literrature_Club.png";
-    tabImg[23] = "images/PP_Arle.png";
-    tabImg[24] = "images/PP_Amitie.png";
-    tabImg[25] = "images/splatoon_callie.png";
-    tabImg[26] = "images/SD_CharlotteSpitz.png";
-    tabImg[27] = "images/Reimu_Hakurei.png";
-    tabImg[28] = "images/Marisa_Kirisame.png";
-    tabImg[29] = "images/Koishi_Komeiji.png";
-    tabImg[30] = "images/Satori_Komeiji.png";
-    tabImg[31] = "images/Flandre_Scarlet.png";
-
-    var lTab = tabImg.length-1;
-
-   document.getElementById('imggirl').src = tabImg[getRandomInt(click)];
-
+function ChangeImgClick() {
+    var Image_Id = document.getElementById('imggirl');
+    if (Image_Id.src.match("images/anime_girls_sexy.png")) {
+        Image_Id.src = "images/ffn_girl.png";
+    }
+    else if (Image_Id.src.match("images/ffn_girl.png")) {
+        Image_Id.src = "images/Sailor_Moon.png";
+    }
+    else if (Image_Id.src.match("images/Sailor_Moon.png")) {
+        Image_Id.src = "images/TMM_Zoey.png";
+    }
+    else if (Image_Id.src.match("images/TMM_Zoey.png")) {
+        Image_Id.src = "images/MD_Doremi.png";
+    }
+    else if (Image_Id.src.match("images/MD_Doremi.png")) {
+        Image_Id.src = "images/SC_Amu.png";
+    }
+    else if (Image_Id.src.match("images/SC_Amu.png")) {
+        Image_Id.src = "images/Kilari.png";
+    }
+    else if (Image_Id.src.match("images/Kilari.png")) {
+        Image_Id.src = "images/Lucie_PPP.png";
+    }
+    else if (Image_Id.src.match("images/Lucie_PPP.png")) {
+        Image_Id.src = "images/FT_Lucy.png";
+    }
+    else if (Image_Id.src.match("images/FT_Lucy.png")) {
+        Image_Id.src = "images/Ayu_Tsukimiya.png";
+    }
+    else if (Image_Id.src.match("images/Ayu_Tsukimiya.png")) {
+        Image_Id.src = "images/mkdm_tohru.png";
+    }
+    else if (Image_Id.src.match("images/mkdm_tohru.png")) {
+        Image_Id.src = "images/The_Quintessential_Quintuplets.png";
+    }
+    else if (Image_Id.src.match("images/The_Quintessential_Quintuplets.png")) {
+        Image_Id.src = "images/Nagatoro.gif";
+    }
+    else if (Image_Id.src.match("images/Nagatoro.gif")) {
+        Image_Id.src = "images/Marin_Kitagawa_MDUD.png";
+    }
+    else if (Image_Id.src.match("images/Marin_Kitagawa_MDUD.png")) {
+        Image_Id.src = "images/Marin_Kitagawa_2_MDUD.png";
+    }
+    else if (Image_Id.src.match("images/Marin_Kitagawa_2_MDUD.png")) {
+        Image_Id.src = "images/Shikimori_Micchon.png";
+    }
+    else if (Image_Id.src.match("images/Shikimori_Micchon.png")) {
+        Image_Id.src = "images/Komi_San.png";
+    }
+    else if (Image_Id.src.match("images/Komi_San.png")) {
+        Image_Id.src = "images/Kizuna_AI.png";
+    }
+    else if (Image_Id.src.match("images/Kizuna_AI.png")) {
+        Image_Id.src = "images/Hatsune_Miku.png";
+    }
+    else if (Image_Id.src.match("images/Hatsune_Miku.png")) {
+        Image_Id.src = "images/Haruhi_Suzumiya.png";
+    }
+    else if (Image_Id.src.match("images/Haruhi_Suzumiya.png")) {
+        Image_Id.src = "images/Konata_Izumi.png";
+    }
+    else if (Image_Id.src.match("images/Konata_Izumi.png")) {
+        Image_Id.src = "images/Doki_Doki_Literrature_Club.png";
+    }
+    else if (Image_Id.src.match("images/Doki_Doki_Literrature_Club.png")) {
+        Image_Id.src = "images/PP_Arle.png";
+    }
+    else if (Image_Id.src.match("images/PP_Arle.png")) {
+        Image_Id.src = "images/PP_Amitie.png";
+    }
+    else if (Image_Id.src.match("images/PP_Amitie.png")) {
+        Image_Id.src = "images/splatoon_callie.png";
+    }
+    else if (Image_Id.src.match("images/splatoon_callie.png")) {
+        Image_Id.src = "images/SD_CharlotteSpitz.png";
+    }
+    else if (Image_Id.src.match("images/SD_CharlotteSpitz.png")) {
+        Image_Id.src = "images/Reimu_Hakurei.png";
+    }
+    else if (Image_Id.src.match("images/Reimu_Hakurei.png")) {
+        Image_Id.src = "images/Marisa_Kirisame.png";
+    }
+    else if (Image_Id.src.match("images/Marisa_Kirisame.png")) {
+        Image_Id.src = "images/Koishi_Komeiji.png";
+    }
+    else if (Image_Id.src.match("images/Koishi_Komeiji.png")) {
+        Image_Id.src = "images/Satori_Komeiji.png";
+    }
+    else if (Image_Id.src.match("images/Satori_Komeiji.png")) {
+        Image_Id.src = "images/Flandre_Scarlet.png";
+    }
+    else {
+        Image_Id.src = "images/anime_girls_sexy.png";
+    }
 }
 
 function somme(input1, input2) {
-           
+
     var operande1 = document.getElementById(input1).value;
     var operande2 = document.getElementById(input2).value;
     var somme = parseFloat(operande1) + parseFloat(operande2);
-    if(isNaN(somme)){alert("Veuillez saisir des nombres");}
-    else{
-    alert("Résultat : " + somme);}
+    if (isNaN(somme)) { alert("Veuillez saisir des nombres"); }
+    else {
+        alert("Résultat : " + somme);
+    }
 }
 
-function randomImgLoad(){
-    var tabImg = new Array ();
+function randomImgLoad() {
+    var tabImg = new Array();
     tabImg[0] = "images/Ciel_Rouge.jpg";
     tabImg[1] = "images/Ciel_Bleu.jpg";
     tabImg[2] = "images/Ciel_Rose.jpg";
 
-    var lTab = tabImg.length-1;
+    var lTab = tabImg.length - 1;
 
-   document.getElementById('Photo_Ciel_Rouge').src = tabImg[getRandomInt(max)];
-   document.getElementById('Photo_Ciel_Bleu').src = tabImg[getRandomInt(max)];
-   document.getElementById('Photo_Ciel_Rose').src = tabImg[getRandomInt(max)];
+    document.getElementById('Photo_Ciel_Rouge').src = tabImg[getRandomInt(max)];
+    document.getElementById('Photo_Ciel_Bleu').src = tabImg[getRandomInt(max)];
+    document.getElementById('Photo_Ciel_Rose').src = tabImg[getRandomInt(max)];
 }
 
 function lien(elem) {
     var reponse = prompt("Veuillez saisir un truc !");
     elem.innerHTML = reponse;
-   }
+}
