cb91bbf184962b6b9ab3470e4d039008769dea23
Fl4m3urDev <VoxFlameur22@gmail.com>
2022-08-06 17:48:11 +0200

    version avec plus ajouts et de correction

---
+++ Page_Vide/script.js
@@ -0,0 +1,13 @@
+// Test fonction JS
+
+window.addEventListener('load', function () {
+    console.log('Cette fonction est éxécutée une fois quand la page est chargée.');
+});
+
+/***************************************************************************************************************/
+
+// Boîte de dialogue (Marrante :P) 
+
+    window.alert("MAIS QU'EST-CE QUE VOUS FAITES ICI !!!");
+
+/***************************************************************************************************************/
