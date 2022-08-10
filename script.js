89d7c402c9c5919e3beac3478594e8e9d93fe708
Fl4m3urDev <VoxFlameur22@gmail.com>
2022-08-10 18:58:52 +0200

    Ajout BTS SIO Main + Correction

--- script.js
+++ script.js
@@ -16,11 +16,11 @@ const max = 3;
 
 var time = new Date().getHours();
 if (time < 7) {
-    window.alert("Bonne nuit et Bienvenue !");
+    window.alert("Bonne nuit et Bienvenue à vous !");
 } else if (time < 18) {
-    window.alert("Bonjour et Bienvenue !");
+    window.alert("Bonjour et Bienvenue à vous !");
 } else {
-    window.alert("Bonsoir et Bienvenue !");
+    window.alert("Bonsoir et Bienvenue à vous !");
 }
 
 /***************************************************************************************************************/
