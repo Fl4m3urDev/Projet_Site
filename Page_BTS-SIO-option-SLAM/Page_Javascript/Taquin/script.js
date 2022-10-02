cc7b0545ef283c8382f9385a5c31e6eafec0a0a1
Fl4m3urDev <VoxFlameur22@gmail.com>
2022-10-02 23:40:45 +0200

    Ajout Exo JS + Taquin

---
+++ Page_BTS-SIO-option-SLAM/Page_Javascript/Taquin/script.js
@@ -0,0 +1,40 @@
+/* on mémorise l'emplacement de la case vide */
+var elig = 4;
+var ecol = 4;
+/* Fonction qui échange la case (lig,col) avec la case vide */
+function move(lig, col) {
+    /* Condition permettant d'echanger la case numéroté par la case vide la plus proche.
+     Par conséquent, les cases passant en diagonale et les cases éloignés de la case vide ne pouvant pas être déplacé.*/
+
+    /* Sans déclaration de variable + Méthode plus courtes et détaillé */
+
+    // if ((elig == lig && ((ecol==col+1) || (ecol==col-1))) || (ecol == col && ((elig == lig-1) || (elig== lig+1)))) {}
+
+    /* Avec déclaration de variable + Méthode plus logique et moins absurdes */
+
+     var case_vide = elig + ecol
+     var case_pleine = lig + col
+     if ((case_vide == case_pleine + 1) || (case_vide == case_pleine - 1)) {
+
+    /* on récupère les identifiants des deux boutons concernés */
+    var bname = 'case' + lig + col;
+    var ename = 'case' + elig + ecol;
+    /* on récupère les noeuds correspondant à ces boutons */
+    var bnode = document.getElementById(bname);
+    var enode = document.getElementById(ename);
+    /* on récupère les fils textuels des deux boutons */
+    var bvalue = bnode.removeChild(bnode.childNodes[0]);
+    var evalue = enode.removeChild(enode.childNodes[0]);
+    /* on échange ces fils */
+    bnode.appendChild(evalue);
+    enode.appendChild(bvalue);
+    /* on échange les classes des deux boutons */
+    bnode.setAttribute('class', 'vide');
+    enode.setAttribute('class', 'case');
+    /* on enlève le "focus" sur le bouton cliqué */
+    bnode.blur();
+    /* on mémorise l'emplacement de la case vide */
+    elig = lig;
+    ecol = col;
+}
+}
