5f54f06d070bf8dbc050bcdca21e2565943d7086
Fl4m3urDev <VoxFlameur22@gmail.com>
2022-11-27 23:50:53 +0100

    correction url (Page : BTS-SIO-option-SLAM)...

--- Page_BTS-SIO-option-SLAM/CanStore_Louis/produits_tri.php
+++ Page_BTS-SIO-option-SLAM/CanStore_Louis/produits_tri.php
@@ -2,9 +2,9 @@
 // header("Content-type: application/json; charset=utf-8");
 // On se connecte, voir cours
 try {
-    $dns = 'mysql:host=localhost;dbname=jardin'; // dbname : nom de la base
-    $utilisateur = 'root'; // root sur vos postes
-    $motDePasse = ''; // pas de mot de passe sur vos postes
+    $dns = 'mysql:host=localhost;dbname=aspt1556_jardin'; // dbname : nom de la base
+    $utilisateur = 'aspt1556_sio'; // root sur vos postes
+    $motDePasse = 'aspt1556_bts'; // pas de mot de passe sur vos postes
     $connexion = new PDO($dns, $utilisateur, $motDePasse);
     $connexion->exec('SET NAMES utf8');
 } catch (Exception $e) {
