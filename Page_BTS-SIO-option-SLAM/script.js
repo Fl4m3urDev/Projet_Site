6ae8e66ff4c89b4407eac60554caec02ba7714f9
Fl4m3urDev <VoxFlameur22@gmail.com>
2023-03-28 23:25:14 +0200

    feat: delete Page_BTS_SIO_Option_SLAM + Move project in Main Page PorteFolio + update testCV => Le_Fameux_CV + add content main page... + Rename Page_Visual_Basic (delete 's') + etc...

--- Page_BTS-SIO-option-SLAM/script.js
+++
@@ -1,7 +0,0 @@
-// Test fonction JS
-
-window.addEventListener('load', function () {
-    console.log('Cette fonction est exécutée une fois quand la page est chargée.');
-  });
-
-/***************************************************************************************************************/
