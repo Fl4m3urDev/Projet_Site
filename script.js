3e3213564ac768aa6aa5e38fa4fa2b751fbceab9
Fl4m3urDev <VoxFlameur22@gmail.com>
2023-04-11 21:46:24 +0200

    delete alert msg JS + add href id...

--- script.js
+++ script.js
@@ -5,16 +5,3 @@ window.addEventListener('load', function () {
 });
 
 /***************************************************************************************************************/
-
-// Boîte de dialogue de Bienvenue (Heure Fixe) 
-
-var time = new Date().getHours();
-if (time < 7) {
-    window.alert("Bonne nuit et Bienvenue à vous !");
-} else if (time < 18) {
-    window.alert("Bonjour et Bienvenue à vous !");
-} else {
-    window.alert("Bonsoir et Bienvenue à vous !");
-}
-
-/***************************************************************************************************************/
